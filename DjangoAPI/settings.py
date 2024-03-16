8ef668a4192774f055a77aaacb0bbbfbc424193e
dev-protim <roypranto445@gmail.com>
2024-03-16 21:30:13 +0100

    Modified.

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'..
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
